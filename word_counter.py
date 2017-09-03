2f2356511f6a9b210d5091eac8f06c6409be8c7c
Andres <andres@Crowds-MacBook-Pro-2.local>
2017-09-03 01:15:21 -0400

    refactored word_counter

--- word_counter.py
+++ word_counter.py
@@ -4,20 +4,15 @@ import sys
 from collections import defaultdict
 
 from helpers.pre_process import read_file
-from map_reduce import MapReduce
+from mapred_engine.map_reduce import MapReduce, MapReduceMultiCore
 
 WORD_RE = re.compile(r"[\w']+")
 
 class WordCounter(MapReduce):
 
-
     def mapper(self, line):
         for word in WORD_RE.findall(line):
             yield word, 1
-
-
-    def combiner(self, entry):
-        return entry
 
 
     def reducer(self, key, entries):
