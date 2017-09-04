fe5608cc4e128cc11dd291bfe9736b252f40c9f3
Andres <andres@Crowds-MacBook-Pro-2.local>
2017-09-04 00:27:18 -0400

    added parse_args helper, requirements and setup.py

---
+++ helpers/parse_args.py
@@ -0,0 +1,27 @@
+import argparse
+from mapred_engine.map_reduce import MapReduce, MapReduceMultiCore
+
+parser = argparse.ArgumentParser(
+        description=
+                    """
+                    Run MapReduce jobs
+                    """
+        )
+
+parser.add_argument('args', nargs='*')
+
+parser.add_argument(
+                    '-m',
+                    required=False,
+                    type=bool,
+                    default=False,
+                    help='Specify running jobs in single/mp mode'
+)
+
+args = vars(parser.parse_args())
+
+def get_map_reduce_class():
+    if args['m']:
+        return MapReduceMultiCore
+    else:
+        return MapReduce
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='mapred_engine',
+    version='0.1',
+    description='Map Reduce Engine Example',
+    packages=[
+        'mapred_engine'
+        ],
+    install_requires=[
+        'PyYAML'
+    ],
+    url='',
+    license='',
+    author='Andres De Castro',
+    author_email='andres@adecastro.ca',
+)
